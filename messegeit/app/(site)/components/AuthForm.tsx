e53972d2afa7cb26c64b3c32859d52643bdbc81b
GnanaHarish <59500003+GnanaHarish@users.noreply.github.com>
2023-07-25 23:23:19 +0530

    Implemented Register Functionality and Next Auth setup
    Implemented Register Functionality and Next Auth setup

--- messegeit/app/(site)/components/AuthForm.tsx
+++ messegeit/app/(site)/components/AuthForm.tsx
@@ -6,6 +6,7 @@ import { BsGithub, BsGoogle } from 'react-icons/bs'
 import { useState, useCallback } from 'react';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import AuthSocialButton from './AuthSocialButton';
+import axios from 'axios';
 
 
 type Variant = 'LOGIN' | 'REGISTER';
@@ -41,6 +42,7 @@ const AuthForm = () => {
         setIsLoading(true);
         if (variant === "REGISTER") {
             //Axios Register
+            axios.post("/api/register", data);
         }
 
         if (variant === "LOGIN") {
