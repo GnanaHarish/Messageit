413a07ff8e01c282485180179025a10980dae327
GnanaHarish <59500003+GnanaHarish@users.noreply.github.com>
2023-07-31 23:52:47 +0530

    Finished Desktop side bar
    Finished Desktop side bar

--- messegeit/app/components/sidebar/DesktopSidebar.tsx
+++ messegeit/app/components/sidebar/DesktopSidebar.tsx
@@ -3,6 +3,7 @@ import useRoutes from "@/app/hooks/useRoutes";
 import { useState } from "react";
 import DesktopItem from "./DesktopItem";
 import { User } from "@prisma/client";
+import Avatar from "../Avatar";
 
 interface DesktopSidebarProps{
     currentUser: User
@@ -12,7 +13,7 @@ const DesktopSidebar: React.FC<DesktopSidebarProps> = ({
     currentUser
 }) => {
     const routes = useRoutes();
-    const [isOpen, setIsOpen] = useState();
+    const [isOpen, setIsOpen] = useState(false);
 
     return (
         <div className="
@@ -50,6 +51,21 @@ const DesktopSidebar: React.FC<DesktopSidebarProps> = ({
                         <DesktopItem key={item.label} href={item.href} label={item.label} icon={item.icon} active={item.active} onClick={item.onClick} />
                     ))}
                 </ul>
+            </nav>
+            <nav className="
+                mt-4
+                flex
+                flex-cols
+                justify-between
+                items-center
+            ">
+                <div onClick = {() => setIsOpen(true)} className="
+                    cursor-pointer
+                    hover:opacity-75
+                    transition
+                ">
+                    <Avatar user={currentUser}/>
+                </div>
             </nav>
         </div>
     );
--- messegeit/next.config.js
+++ messegeit/next.config.js
@@ -8,6 +8,13 @@ const nextConfig = {
                 {}
             ]
         ]
+    },
+    images: {
+        domains: [
+            "res.cloudinary.com",
+            "avatars.githubusercontent.com",
+            "lh3.googleusercontent.com"
+        ]
     }
 }
 
