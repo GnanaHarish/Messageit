d524183fd673f7a8b6aef38d8f18224cbbfec7f8
GnanaHarish <59500003+GnanaHarish@users.noreply.github.com>
2023-08-06 19:21:52 +0530

    worked on conversation part
    worked on conversation part

---
+++ messegeit/app/actions/getMessages.ts
@@ -0,0 +1,26 @@
+import prisma from '@/app/libs/prismadb';
+
+const getMessages = async (
+    conversationId : string
+) => {
+    try {
+        const messages = await prisma.message.findMany({
+            where: {
+                conversationIds: conversationId
+            },
+            include:{
+                seen: true,
+                sender: true
+            },
+            orderBy:{
+                createdAt: 'asc'
+            }
+        });
+
+        return messages;
+    } catch (error: any) {
+        return [];
+    }
+}
+
+export default getMessages;
